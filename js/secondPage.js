fce88aa753696b30b2df8e6a4cc117f9da0dff49
ahmedabdelfattah50 <ahmedabdelfatah661540@gmail.com>
2021-08-27 20:09:58 +0200

    first upload

---
+++ js/secondPage.js
@@ -0,0 +1,13 @@
+
+var vm = new Vue({
+    el: "#myApp",
+    data: {
+        name: "Ahmed",
+        age: '',
+        skills: [
+            'HTML' , 
+            'CSS' ,
+            'JS'
+        ],
+    }
+})
