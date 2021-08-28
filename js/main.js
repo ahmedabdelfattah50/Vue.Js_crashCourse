2a4b1ebdee2c19552799318392125488b558bae0
ahmedabdelfattah50 <ahmedabdelfatah661540@gmail.com>
2021-08-28 11:37:41 +0200

    after app 2

--- js/main.js
+++ js/main.js
@@ -12,3 +12,28 @@ var vm = new Vue({
     }
 });
 // ========= End myApp_1 Vue.Js =========
+
+// ========= Start myApp_2 Vue.Js =========
+var vm = new Vue({
+    el: "#myApp_2",
+    data: {
+        myId: "testBinding",
+        type: "M",
+        counter: 0,
+        counterStop: ""
+    },
+    methods: {
+        increaseFun: function(){
+            this.counterStop = ""
+            this.counter++;
+        },
+        decreaseFun: function(){
+            if(this.counter != 0){
+                this.counter--;
+            } else {
+                this.counterStop = "Sorry, The small number is (0)"
+            }
+        }
+    }
+});
+// ========= End myApp_2 Vue.Js =========
